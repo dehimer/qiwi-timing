c46f269ad571440a56764780796915f85912aae6
Denis Novikov <den-vse@yandex.ru>
2019-04-17 21:43:57 +0300

    enhancement: update view of right columnt with qrs

--- src/client/components/BigScreen/index.js
+++ src/client/components/BigScreen/index.js
@@ -30,125 +30,135 @@ class BigScreen extends PureComponent {
     const currentSpeaker = timing.find(({id}) => id === currentSpeakerId);
 
     const buzySpaces = [];
-    const findFreePosition = (size) => {
-      const top = (100-size)*Math.random();
-      const left = (100-size)*Math.random();
+    const findFreePosition = (sizeV, sizeH, shortWidth) => {
+      const top = (100-sizeV)*Math.random();
+      const left = (shortWidth-sizeH)*Math.random();
 
       const foundOverlay = buzySpaces.find(space => {
         return !(
-          space.left > (left + size) ||
-          (space.left + space.size) < left ||
-          space.top > (top + size) ||
-          (space.top + space.size) < top
+          space.left > (left + sizeH) ||
+          (space.left + space.sizeH) < left ||
+          space.top > (top + sizeV) ||
+          (space.top + space.sizeV) < top
         );
       });
 
       if (foundOverlay) {
-        return findFreePosition(size);
+        return findFreePosition(sizeV, sizeH, shortWidth);
       } else {
         return [top, left];
       }
 
     };
 
-    const content = timing.filter(({speaker}) => !!speaker).map(({ id, time, speaker, theme, color, photo, description }) => {
-        const isSelected = id === currentSpeakerId;
-
-        const sizeCoefficient = 1;/* + Math.random();*/
-        const size = isSelected ? 100 : 15 * sizeCoefficient;
-        const fontSize = size;
-
-        // console.log(`${id} === ${currentSpeakerId} ${isSelected}`);
-        let top = 0;
-        let left = 0;
-
-        if (!isSelected) {
-          // console.log('findFreePosition');
-          [top, left] = findFreePosition(size*0.9);
-
-          buzySpaces.push({
-            id,
-            top,
-            left,
-            size
-          });
-        }
-
-        const speakerStyles = {
-          backgroundColor: hexToRgba(color, isSelected ? 0.5 : 0.1),
-          opacity: !isSelected && currentSpeakerId && currentSpeaker.speaker ? 0 : 1,
-          zIndex: isSelected ? 10: 1,
-          height: `${size}vh`,
-          width: `${size}vw`,
-          top: `${top}vh`,
-          left: `${left}vw`,
-          fontSize: `${fontSize}px`
-
-        };
-
-        const rayTopStyles = {
-          backgroundColor: color,
-          opacity: 0.1,
-          width: `${size}vw`,
-          height:  `${Math.abs(50-top)}vh`,
-          top: top < 50 ? `${top}vh` : `50vh`,
-          left: `${left}vw`,
-          // transform: `skewX(${(left+size/2)-50}deg)`
-        };
-
-        const rayLeftStyles = {
-          backgroundColor: color,
-          opacity: 0.1,
-          height:  `${size}vh`,
-          width:  `${Math.abs(50-left)}vw`,
-          top: `${top}vh`,
-          left: left < 50 ? `${left}vw` : `50vw`,
-          // transform: `skewX(${(left+size/2)-50}deg)`
-        };
-
-        return (
-          <Fragment key={id} >
-            <div className="speaker" style={speakerStyles}>
-              <div
-                className="long"
-                style={
-                  isSelected
-                    ? {height: '100%', opacity: 1}
-                    : {height: '0', opacity: 0}
-                }
-              >
-                <div className="photoqrs">
-                  { photo && <img src={`/public/speakers/${photo}`} alt="photo"/> }
-                  <img src={`/public/qrs/qr-qiwi-events-tim.svg`} alt="qrqiwi"/>
-                  <img src={`/public/qrs/qr-habr-com.svg`} alt="qrhabr"/>
-                  <img src={`/public/qrs/qr-youtube-com.svg`} alt="qryoutube"/>
-                </div>
-                { description && <div className="description">{description}</div> }
-
-              </div>
-
-              <div
-                className="short"
-                style={
-                  !isSelected
-                    ? {height: '100%', opacity: 1}
-                    : {height: '0', opacity: 0}
-                }
-              >
-                <div className="time"><span>{time[0]}</span> - <span>{time[1]}</span></div>
-                <div className="person">{speaker}</div>
-                <div className="point">{theme}</div>
-              </div>
-            </div>
-            { !isSelected && <div className="ray-top" style={rayTopStyles}/> }
-            { !isSelected && <div className="ray-left" style={rayLeftStyles}/> }
-          </Fragment>
-        )
-      });
-
     return (
       <div className="bigscreen">
-        { content }
+        <div className="timing">
+          {
+            timing.filter(({speaker}) => !!speaker).map(({ id, time, speaker, theme, color, photo, description }) => {
+              const isSelected = id === currentSpeakerId;
+
+              // const sizeCoefficient = 1;/* + Math.random();*/
+              const shortWidth = 84;
+              const sizeH = isSelected ? shortWidth : 15;
+              const sizeV = isSelected ? 100 : 16;
+              const fontSize = sizeV;
+
+              // console.log(`${id} === ${currentSpeakerId} ${isSelected}`);
+              let top = 0;
+              let left = 0;
+
+              if (!isSelected) {
+                // console.log('findFreePosition');
+                [top, left] = findFreePosition(sizeV*0.9, sizeH*0.9, shortWidth);
+
+                buzySpaces.push({
+                  id,
+                  top,
+                  left,
+                  sizeV,
+                  sizeH
+                });
+              }
+
+              const speakerStyles = {
+                backgroundColor: hexToRgba(color, isSelected ? 0.5 : 0.1),
+                opacity: !isSelected && currentSpeakerId && currentSpeaker.speaker ? 0 : 1,
+                zIndex: isSelected ? 10: 1,
+                height: `${sizeV}vh`,
+                width: `${sizeH}vw`,
+                top: `${top}vh`,
+                left: `${left}vw`,
+                fontSize: `${fontSize}px`
+
+              };
+
+              const rayTopStyles = {
+                backgroundColor: color,
+                opacity: 0.1,
+                width: `${sizeH}vw`,
+                height:  `${Math.abs(50-top)}vh`,
+                top: top < 50 ? `${top}vh` : `50vh`,
+                left: `${left}vw`,
+                // transform: `skewX(${(left+size/2)-50}deg)`
+              };
+
+              const rayLeftStyles = {
+                backgroundColor: color,
+                opacity: 0.1,
+                height:  `${sizeV}vh`,
+                width:  `${Math.abs(shortWidth/2-left)}vw`,
+                top: `${top}vh`,
+                left: left < shortWidth/2 ? `${left}vw` : `${shortWidth/2}vw`,
+                // transform: `skewX(${(left+size/2)-50}deg)`
+              };
+
+              return (
+                <Fragment key={id} >
+                  <div className="speaker" style={speakerStyles}>
+                    <div
+                      className="long"
+                      style={
+                        isSelected
+                          ? {height: '100%', opacity: 1}
+                          : {height: '0', opacity: 0}
+                      }
+                    >
+                      <div className="photoqrs">
+                        { photo && <img src={`/public/speakers/${photo}`} alt="photo"/> }
+                      </div>
+                      { description && <div className="description">{description}</div> }
+
+                    </div>
+
+                    <div
+                      className="short"
+                      style={
+                        !isSelected
+                          ? {height: '100%', opacity: 1}
+                          : {height: '0', opacity: 0}
+                      }
+                    >
+                      <div className="time"><span>{time[0]}</span> - <span>{time[1]}</span></div>
+                      <div className="person">{speaker}</div>
+                      <div className="point">{theme}</div>
+                    </div>
+                  </div>
+                  { !isSelected && <div className="ray-top" style={rayTopStyles}/> }
+                  { !isSelected && <div className="ray-left" style={rayLeftStyles}/> }
+                </Fragment>
+              )
+            })
+          }
+        </div>
+        <div className="qrs">
+
+          <img alt='back' src={`/public/qiwi_logo_cmyk.svg`} className="qiwilogo" />
+
+          <img src={`/public/qrs/qr-qiwi-events-tim.svg`} alt="qrqiwi"/>
+          <img src={`/public/qrs/qr-habr-com.svg`} alt="qrhabr"/>
+          <img src={`/public/qrs/qr-youtube-com.svg`} alt="qryoutube"/>
+        </div>
       </div>
     );
   }
